3f552ff263c36925f4b4b1a7496023057f481c3e
Itachikunn <160922473+Itachikunn@users.noreply.github.com>
2024-04-16 07:00:07 +0530

    config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from apdbot.sample_con import Config
 
 class Config(Config):
   BOT_TOKEN = "6588896950:AAEkNoXi2NT9CMaaNdaePEDvE-1prm-arb0"
-  APP_ID = 24339011
+  APP_ID = "24339011"
   API_HASH = "a85d1d917af0d4d02811a9a007b8dcda"
   AUTH = [-4169002529]
