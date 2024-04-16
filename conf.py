08c4e221baae3989394bdcd94aa719cb4e3f2e90
Itachikunn <160922473+Itachikunn@users.noreply.github.com>
2024-04-16 06:54:38 +0530

    conf.py

---
+++ conf.py
@@ -0,0 +1,7 @@
+from apdbot.sample_con import Config
+
+class Config(Config):
+  BOT_TOKEN = "6588896950:AAEkNoXi2NT9CMaaNdaePEDvE-1prm-arb0"
+  API_ID = 24339011
+  API_HASH = "a85d1d917af0d4d02811a9a007b8dcda"
+  AUTH = [-4169002529]
